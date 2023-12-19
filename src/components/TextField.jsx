3ec5ba72331069cc89de56063655cf652a575c6f
Moises Lopez <moiseslopez@MoiseM.local>
2023-12-19 12:51:34 -0600

    Input

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,35 +1,45 @@
 import React from 'react';
-import { Grid, TextField } from '@mui/material';
+import { FormControl, Grid, InputLabel, OutlinedInput, Typography } from '@mui/material';
 
 const CustomTextField = ({
   label,
   name,
-  defaultValue,
+  errors={},
+  onChange,
+  placeholder,
+  type
 }) => {
   return (
-    <Grid item xs={12} sm={12} md={12} lg={12} xl={12} margin={2}>
-      <TextField
-        variant="outlined"
-        type="text"
-        autoFocus
-        fullWidth
-        required
-        name={name}
-        label={label}
-        InputLabelProps={{
-          style: {
+    <Grid container item xs={12} sm={12} md={12} lg={12} xl={12} margin={3} style={{ textAlign: 'left' }} flexDirection={'column'} justifyContent={'center'}>
+      <Typography variant="body2" color={'black'} style={{ fontSize: '1.5rem', fontFamily: 'Montserrat', fontWeight: 'bolder' }}>{label}</Typography>
+      <FormControl sx={{ m: 0, width: '100%', alignSelf: 'center', background: 'white', borderRadius: 10 }} variant="solid">
+        <OutlinedInput
+          id="outlined-adornment-weight"
+          placeholder={placeholder}
+          aria-describedby="outlined-weight-helper-text"
+          color={'secondary'}
+          size='large'
+          inputProps={{
+            'aria-label': 'weight',
+          }}
+          onChange={onChange}
+          type={type}
+          sx={{
+            borderRadius: 10,
+            borderColor: 'secondary',
             fontSize: '1.5rem',
-          }
-        }}
-        color="secondary"
-        InputProps={{
-          sx: {
-            borderRadius: 10, fontFamily: 'Montserrat',
-            fontSize: '1.5rem'
-          }
-        }}
-        defaultValue={defaultValue}
-      />
+            fontFamily: 'Montserrat'
+          }}
+        />
+        
+      </FormControl>
+      {errors[name] &&
+          <Typography variant="body2" color={'red'} style={{ fontSize: '1.5rem', fontFamily: 'Montserrat'}}>
+            {
+              errors[name]
+            }
+          </Typography>
+        }
     </Grid>
   );
 };
